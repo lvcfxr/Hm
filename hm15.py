cbf4ff999fc994e5c41b7c8c6caeeff96dec70a6
Valerii <wateere@gmail.com>
2024-02-29 13:13:26 +0100

    FifthCommit

--- hm15.py
+++ hm15.py
@@ -3,15 +3,15 @@ from forex_python.converter import CurrencyRates
 def convert_currency(amount, from_currency, to_currency):
     exchange_rates = {
         'USD': {
-            'EUR': 0.89,
-            'UAH': 27.50
+            'EUR': 0.92,
+            'UAH': 38.11
         },
         'EUR': {
-            'USD': 1.12,
-            'UAH': 31.00
+            'USD': 1.08,
+            'UAH': 41.28
         },
         'UAH': {
-            'USD': 0.036,
-            'EUR': 0.032
+            'USD': 0.026,
+            'EUR': 0.024
         }
     }
