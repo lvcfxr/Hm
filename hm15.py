94f34784811d9050794e97957e27f7883f4b5cef
Valerii <wateere@gmail.com>
2024-02-29 13:06:52 +0100

    ThirdCommit

--- hm15.py
+++ hm15.py
@@ -1,4 +1,10 @@
 from forex_python.converter import CurrencyRates
 
 def convert_currency(amount, from_currency, to_currency):
-    pass
+    c = CurrencyRates()
+    result = c.convert(from_currency, to_currency, amount)
+    return result
+
+amount = float(input("Введите сумму для конвертации: "))
+from_currency = input("Введите код валюты, из которой хотите конвертировать: ").upper()
+to_currency = input("Введите код валюты, в которую хотите конвертировать: ").upper()
