b9c4c6b4456a0598f21b703d790c15dfb56fd4ba
Valerii <wateere@gmail.com>
2024-02-29 13:16:49 +0100

    FinalCommit

--- hm15.py
+++ hm15.py
@@ -1,5 +1,5 @@
-from forex_python.converter import CurrencyRates
 
+#Курс валют на момент 29.2.2024
 def convert_currency(amount, from_currency, to_currency):
     exchange_rates = {
         'USD': {
@@ -15,7 +15,7 @@ def convert_currency(amount, from_currency, to_currency):
             'EUR': 0.024
         }
     }
-    
+
     if from_currency == to_currency:
         return amount
 
@@ -25,3 +25,13 @@ def convert_currency(amount, from_currency, to_currency):
     rate = exchange_rates[from_currency][to_currency]
     converted_amount = amount * rate
     return converted_amount
+
+amount = float(input("Введите сумму для конвертации: "))
+from_currency = input("Введите код валюты, из которой хотите конвертировать (USD, EUR, UAH): ").upper()
+to_currency = input("Введите код валюты, в которую хотите конвертировать (USD, EUR, UAH): ").upper()
+
+converted_amount = convert_currency(amount, from_currency, to_currency)
+if converted_amount is None:
+    print("Курс валют не найден.")
+else:
+    print(f'{amount} {from_currency} = {converted_amount} {to_currency}')
